baa87ce657580381ec6279978fd793ad043b1e95
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-08 10:38:35 +0200

    created schema for data structure

--- source/odm/todo.js
+++ source/odm/todo.js
@@ -2,7 +2,32 @@
 import mongoose from 'mongoose';
 
 // Document shape
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+    message: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+    },
+    favorite: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+}, { timestamps: {createdAt: 'created', updatedAt: 'modified'}});
+
+// {
+//     message: 'My task',
+//     favorite: false,
+//     completed: false,
+//     created: 'Date',
+//     modified: 'Data'
+// }
 
 // Collection
 export const todo = mongoose.model('todos', schema);
