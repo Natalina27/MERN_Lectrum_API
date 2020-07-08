cd1d675f41864e7a8c127eb0f99c69bc8db3984c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-08 11:48:12 +0200

    created handler for read in routes finished realization of read feature

--- source/routes/todo/index.js
+++ source/routes/todo/index.js
@@ -1,9 +1,18 @@
 // Instruments
 import { Todo } from '../../controllers';
 
-export const read = (req, res) => {
+export const read =  async(req, res) => {
     try {
-        res.status(200).json({ message: 'Welcome to workshop!' });
+        const todo = new Todo(); //создаем экземпляр контроллера
+
+
+        //у контроллера мы вызываем метод read,
+        // контроллер вызывает модель
+        //модель извлекает данные из базы
+        //и возвращает все в переменную data
+        const data = await todo.read();
+
+        res.status(200).json({data: data});
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
